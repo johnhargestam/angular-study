5e5e5b8b656059bf9c422b004f101bbc8d8e9f81
= <john.hargestam@gmail.com>
2021-08-10 09:53:00 +0200

    complete The Hero Editor tutorial step

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Hero {
+  id: number;
+  name: string;
+}
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
